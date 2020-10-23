0b3a2912d1772dd84e5edc21d827cdd85ce03913
Cooper Saye <coopersaye@gmail.com>
2020-10-23 14:44:16 -0700

    Implement inventory toggling.

---
+++ Assets/Inventory/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Inventory
+{
+    public class UIManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject menuCanvas = null;
+
+        void Update()
+        {
+            // If escape key pressed, toggle menu UI
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menuCanvas.SetActive(!menuCanvas.activeSelf);
+            }
+        }
+    }
+}
