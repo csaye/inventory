7e0bd73ca7371ad7cbdf58f431ef2c22679be3a3
Cooper Saye <coopersaye@gmail.com>
2020-10-23 21:54:58 -0700

    Implement hotbar slot highlighting.

---
+++ Assets/Inventory/Scripts/Hotbar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Inventory
+{
+    public class Hotbar : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private HotbarSlot[] slots = new HotbarSlot[10];
+
+        private Color normalColor = Color.white;
+        private Color selectedColor = new Color(0.8f, 0.8f, 0.8f);
+
+        private HotbarSlot _selectedSlot = null;
+        public HotbarSlot selectedSlot
+        {
+            get { return _selectedSlot; }
+            set
+            {
+                if (selectedSlot != null) selectedSlot.SetSlotColor(normalColor);
+                _selectedSlot = value;
+                selectedSlot.SetSlotColor(selectedColor);
+            }
+        }
+
+        // Initialize with slot zero selected
+        private void Start()
+        {
+            selectedSlot = slots[0];
+        }
+    }
+}
---
+++ Assets/Inventory/Scripts/HotbarSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Inventory
+{
+    public class HotbarSlot : InventorySlot
+    {
+        [Header("Hotbar Slot References")]
+        [SerializeField] private Image panel = null;
+        [SerializeField] private Hotbar hotbar = null;
+
+        public void HighlightSlot() => hotbar.selectedSlot = this;
+
+        public void SetSlotColor(Color color) => panel.color = color;
+    }
+}
